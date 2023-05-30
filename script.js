eaf231c38feed3d1b111a24c00b68185285f9e01
oscarmedina234 <oscarmedina234@gmail.com>
2023-05-29 21:24:38 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+// This gets the current day and adds it to the website 
+let whatsTheDay = dayjs().format('ddd MMM DD, YYYY  h:mm a');
+let showDate = document.querySelector('#currentDay');
+showDate.textContent = whatsTheDay
+
+$(document).ready(function () {
+  
+// this gets the event and hour and saves it to local storage when the save button is clicked. 
+  $('.saveBtn').on('click', function(){
+    let event = $(this).siblings('.description').val();
+    let hour = $(this).parent().attr('id');
+
+    localStorage.setItem(hour, event);
+  });
+  
+// This updates the color of the hour block to match it with the right style class depending on the time. 
+  function updateColor() {
+    let theHourIs = dayjs().hour();
+
+    $('.time-block').each(function (){
+      let idHour = parseInt($(this).attr('id').split('-')[1]);
+
+      if (idHour < theHourIs){
+        $(this).addClass('past');
+      } else if (idHour === theHourIs) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+  updateColor();
+
+  //This gets anything that was saved in local storage and displays it on the website
+function recallEvent() {
+  
+  $('.time-block').each(function () {
+    let blockId = $(this).attr('id');
+    $(this).children('.description').val(localStorage.getItem(blockId));
+  });
+};
+
+recallEvent();
+
 });
